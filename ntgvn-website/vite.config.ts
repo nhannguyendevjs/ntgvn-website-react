2a2f10eef26244d3bdc4e5f72ad6866a3a92ad7a
Nhan Nguyen <hoainhaannguyen@gmail.com>
2023-09-16 19:28:43 +0700

    Add PWA

--- ntgvn-website/vite.config.ts
+++ ntgvn-website/vite.config.ts
@@ -1,21 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { compression } from 'vite-plugin-compression2';
+import { compression } from 'vite-plugin-compression2';
+import { VitePWA } from 'vite-plugin-pwa';
+import { manifestForPlugIn } from './manifest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // compression({
-    //   algorithm: 'gzip',
-    //   exclude: [/\.(br)$ /, /\.(gz)$/],
-    //   // deleteOriginalAssets: true,
-    // }),
+    compression({
+      algorithm: 'gzip',
+      exclude: [/\.(br)$ /, /\.(gz)$/],
+      deleteOriginalAssets: true,
+    }),
     // // compression({
     // //   algorithm: 'brotliCompress',
     // //   exclude: [/\.(br)$ /, /\.(gz)$/],
     // //   // deleteOriginalAssets: true,
     // // }),
+    VitePWA(manifestForPlugIn),
   ],
   server: {
     port: 4200,
