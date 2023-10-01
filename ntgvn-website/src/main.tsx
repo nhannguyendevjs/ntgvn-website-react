ae18ff8ea7e733348dd5036527d2a51e15b28e76
Nhan Nguyen <hoainhaannguyen@gmail.com>
2023-10-01 15:28:06 +0700

    Fix build

--- ntgvn-website/src/main.tsx
+++ ntgvn-website/src/main.tsx
@@ -2,10 +2,18 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './style.scss';
 import router from './router';
+import GLOBAL_SETTINGS from './global-settings';
+import React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-  <RouterProvider router={router} />
-  // </React.StrictMode>
-);
+if (GLOBAL_SETTINGS.production) {
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <RouterProvider router={router} />
+  );
+}
